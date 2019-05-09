3a8fec4d8c4f02caa85057f5f46f97204e8b7a8a
kiko0701 <sol.tidus.rr@gmail.com>
2019-05-10 00:38:09 +0900

    yarnプロジェクトに更新。vuetify削除

---
+++ src/components/header-nav-bar/header-nav-bar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-content class="header-container">
+      <v-toolbar 
+        dark 
+        tabs>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>{{title}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-content>
+  </div>
+</template>
+<!-- file import -->
+<script lang="ts" src="./header-nav-bar.ts"></script>
+<style lang="scss" scoped src="./header-nav-bar.scss"></style>
---
+++ src/views/laugh-page/laugh-page.vue
@@ -0,0 +1,26 @@
+<template>
+<v-container fluid justify-space-around>
+  <div class="makelaugh-container">
+    <div class="title-container"><span>オススメ</span></div>
+    <div class="item-container">
+      <v-flex 
+        v-for="themeItem in themeItems"
+        :key="themeItem.id"
+        :themeItem="themeItem"
+        style="margin: 15px;">
+        <theme-item
+        :key="themeItem.id"
+        :themeItem="themeItem"
+        :footerMenuType="footerMenuType">
+        </theme-item>
+      </v-flex>
+    </div>
+  </div>
+  <div class="laugh-container"></div>
+  <div class="ranking-container"></div>
+</v-container>
+</template>
+<!-- File Import -->
+<script lang="ts" src="./laugh-page.ts"></script>
+<style lang="scss" scoped src="./laugh-page.scss"></style>
+
